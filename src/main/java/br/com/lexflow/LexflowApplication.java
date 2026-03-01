17624730085fefb93ba53179bb8562e9187692a0
lelyrv <lely.r.v@hotmail.com>
2026-03-01 01:31:46 -0300

    feat: initial Spring Boot project scaffold

---
+++ src/main/java/br/com/lexflow/LexflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.lexflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lexflow/LexflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lexflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
